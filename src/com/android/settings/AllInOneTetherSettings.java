NAMELESS-AOSP-OPENSOURCE/packages_apps_Settings

SoftAp: add client manager
Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
Change-Id: If9f0c1000ff4e7dd0b602a61299f1eb2c7608ac5

src/com/android/settings/AllInOneTetherSettings.java
@@ -62,6 +62,7 @@ import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
 import ink.kscope.settings.wifi.tether.WifiTetherHiddenSsidPreferenceController;
 import ink.kscope.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
+import ink.kscope.settings.wifi.tether.WifiTetherClientManagerPreferenceController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -100,6 +101,9 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
     public static final String BLUETOOTH_TETHER_KEY = "enable_bluetooth_tethering" + DEDUP_POSTFIX;
     public static final String ETHERNET_TETHER_KEY = "enable_ethernet_tethering" + DEDUP_POSTFIX;
 
+    static final String KEY_WIFI_TETHER_CLIENT_MANAGER =
+            WifiTetherClientManagerPreferenceController.PREF_KEY + DEDUP_POSTFIX;
+
     @VisibleForTesting
     static final int EXPANDED_CHILD_COUNT_DEFAULT = 4;
     @VisibleForTesting
@@ -124,6 +128,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
     private WifiTetherSecurityPreferenceController mSecurityPreferenceController;
     private WifiTetherHiddenSsidPreferenceController mHiddenSsidPreferenceController;
     private WifiTetherAutoOffPreferenceController mAutoOffPreferenceController;
+    private WifiTetherClientManagerPreferenceController mClientPreferenceController;
     private PreferenceGroup mWifiTetherGroup;
     private boolean mShouldShowWifiConfig = true;
     private boolean mHasShownAdvance;
@@ -194,6 +199,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         mApBandPreferenceController = use(WifiTetherApBandPreferenceController.class);
         mHiddenSsidPreferenceController = use(WifiTetherHiddenSsidPreferenceController.class);
         mAutoOffPreferenceController = use(WifiTetherAutoOffPreferenceController.class);
+        mClientPreferenceController = use(WifiTetherClientManagerPreferenceController.class);
         getSettingsLifecycle().addObserver(use(UsbTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(BluetoothTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(EthernetTetherPreferenceController.class));
@@ -342,6 +348,8 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                 new WifiTetherHiddenSsidPreferenceController(context, listener));
         controllers.add(
                 new WifiTetherFooterPreferenceController(context));
+        controllers.add(
+                new WifiTetherClientManagerPreferenceController(context, listener));
 
         return controllers;
     }
@@ -387,6 +395,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         }
         mApBandPreferenceController.setupBands(configBuilder);
         mAutoOffPreferenceController.updateConfig(configBuilder);
+        mClientPreferenceController.updateConfig(configBuilder);
         configBuilder.setHiddenSsid(mHiddenSsidPreferenceController.isHiddenSsidEnabled());
         return configBuilder.build();
     }
@@ -435,6 +444,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                         keys.add(KEY_WIFI_TETHER_AUTO_OFF);
                         keys.add(KEY_WIFI_TETHER_NETWORK_AP_BAND);
                         keys.add(KEY_WIFI_TETHER_SECURITY);
+                        keys.add(KEY_WIFI_TETHER_CLIENT_MANAGER);
                     }
                     return keys;
                 }

src/ink/kscope/settings/wifi/tether/WifiTetherClientManagerPreferenceController.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ink.kscope.settings.wifi.tether;
+
+import android.annotation.NonNull;
+import android.content.Context;
+import android.net.MacAddress;
+import android.net.wifi.SoftApCapability;
+import android.net.wifi.SoftApConfiguration;
+import android.net.wifi.WifiManager;
+import android.util.FeatureFlagUtils;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.FeatureFlags;
+import com.android.settings.wifi.tether.WifiTetherBasePreferenceController;
+
+import java.util.List;
+
+public class WifiTetherClientManagerPreferenceController extends WifiTetherBasePreferenceController 
+        implements WifiManager.SoftApCallback {
+
+    public static final String DEDUP_POSTFIX = "_2";
+    public static final String PREF_KEY = "wifi_tether_client_manager";
+
+    private boolean mSupportForceDisconnect;
+
+    public WifiTetherClientManagerPreferenceController(Context context,
+            WifiTetherBasePreferenceController.OnTetherConfigUpdateListener listener) {
+        super(context, listener);
+
+        mWifiManager.registerSoftApCallback(context.getMainExecutor(), this);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return FeatureFlagUtils.isEnabled(mContext, FeatureFlags.TETHER_ALL_IN_ONE)
+                ? PREF_KEY + DEDUP_POSTFIX : PREF_KEY;
+    }
+
+    @Override
+    public void onCapabilityChanged(@NonNull SoftApCapability softApCapability) {
+        mSupportForceDisconnect =
+                softApCapability.areFeaturesSupported(SoftApCapability.SOFTAP_FEATURE_CLIENT_FORCE_DISCONNECT);
+        mWifiManager.unregisterSoftApCallback(this);
+        updateDisplay();
+    }
+
+    @Override
+    public void updateDisplay() {
+        if (mPreference != null) {
+            if (mSupportForceDisconnect) {
+                mPreference.setSummary(R.string.wifi_hotspot_client_manager_summary);
+            } else {
+                mPreference.setSummary(R.string.wifi_hotspot_client_manager_list_only_summary);
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
+    }
+
+    public void updateConfig(SoftApConfiguration.Builder builder) {
+        if (builder == null || !mSupportForceDisconnect) return;
+        final SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
+        final int maxNumberOfClients = softApConfiguration.getMaxNumberOfClients();
+        final List<MacAddress> blockedClientList = softApConfiguration.getBlockedClientList();
+        builder.setMaxNumberOfClients(maxNumberOfClients)
+                .setBlockedClientList(blockedClientList);
+    }
+}

src/ink/kscope/settings/wifi/tether/preference/WifiTetherClientLimitPreference.java
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ink.kscope.settings.wifi.tether.preference;
+
+import android.content.Context;
+import android.widget.SeekBar;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.settings.R;
+import com.android.settings.SeekBarDialogPreference;
+
+public class WifiTetherClientLimitPreference extends SeekBarDialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private Context mContext;
+    private SeekBar mSeekBar;
+    private int mValue;
+    private int mMin;
+    private int mMax;
+
+    public WifiTetherClientLimitPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        setText(getSummaryForValue(progress + mMin));
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mSeekBar = getSeekBar(view);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mSeekBar.setMax(mMax - mMin);
+        mSeekBar.setProgress(mValue - mMin);
+    }
+
+    private String getSummaryForValue(int value) {
+        return mContext.getResources().getQuantityString(R.plurals.wifi_hotspot_client_limit_summary, value, value);
+    }
+
+    public void setMin(int min) {
+        mMin = min;
+    }
+
+    public void setMax(int max) {
+        mMax = max;
+    }
+
+    public void setValue(int value, boolean callListener) {
+        if (value == 0) value = mMax;
+        mValue = value;
+        String summary = getSummaryForValue(value);
+        setSummary(summary);
+        setText(summary);
+        if (callListener) callChangeListener(value);
+    }
+
+    public int getValue() {
+        return mValue;
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            setValue(mSeekBar.getProgress() + mMin, true);
+        }
+    }
+}

rSM6150/awaken_packages_apps_Settings

src/com/android/settings/AllInOneTetherSettings.java
@@ -54,6 +54,7 @@ import com.android.settings.widget.SettingsMainSwitchBar;
 import com.android.settings.wifi.tether.WifiTetherApBandPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherBasePreferenceController;
+import com.android.settings.wifi.tether.WifiTetherClientManagerPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherFooterPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherHiddenSsidPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherPasswordPreferenceController;
@@ -100,6 +101,9 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
     public static final String BLUETOOTH_TETHER_KEY = "enable_bluetooth_tethering" + DEDUP_POSTFIX;
     public static final String ETHERNET_TETHER_KEY = "enable_ethernet_tethering" + DEDUP_POSTFIX;
 
+    static final String KEY_WIFI_TETHER_CLIENT_MANAGER =
+            WifiTetherClientManagerPreferenceController.PREF_KEY + DEDUP_POSTFIX;
+
     @VisibleForTesting
     static final int EXPANDED_CHILD_COUNT_DEFAULT = 4;
     @VisibleForTesting
@@ -124,6 +128,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
     private WifiTetherSecurityPreferenceController mSecurityPreferenceController;
     private WifiTetherHiddenSsidPreferenceController mHiddenSsidPreferenceController;
     private WifiTetherAutoOffPreferenceController mAutoOffPreferenceController;
+    private WifiTetherClientManagerPreferenceController mClientPreferenceController;
     private PreferenceGroup mWifiTetherGroup;
     private boolean mShouldShowWifiConfig = true;
     private boolean mHasShownAdvance;
@@ -194,6 +199,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         mApBandPreferenceController = use(WifiTetherApBandPreferenceController.class);
         mHiddenSsidPreferenceController = use(WifiTetherHiddenSsidPreferenceController.class);
         mAutoOffPreferenceController = use(WifiTetherAutoOffPreferenceController.class);
+        mClientPreferenceController = use(WifiTetherClientManagerPreferenceController.class);
         getSettingsLifecycle().addObserver(use(UsbTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(BluetoothTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(EthernetTetherPreferenceController.class));
@@ -345,6 +351,8 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                 new WifiTetherHiddenSsidPreferenceController(context, listener));
         controllers.add(
                 new WifiTetherFooterPreferenceController(context));
+        controllers.add(
+                new WifiTetherClientManagerPreferenceController(context, listener));
 
         return controllers;
     }
@@ -390,6 +398,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         }
         mApBandPreferenceController.setupBands(configBuilder);
         mAutoOffPreferenceController.updateConfig(configBuilder);
+        mClientPreferenceController.updateConfig(configBuilder);
         configBuilder.setHiddenSsid(mHiddenSsidPreferenceController.isHiddenSsidEnabled());
         return configBuilder.build();
     }
@@ -438,6 +447,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                         keys.add(KEY_WIFI_TETHER_AUTO_OFF);
                         keys.add(KEY_WIFI_TETHER_NETWORK_AP_BAND);
                         keys.add(KEY_WIFI_TETHER_SECURITY);
+                        keys.add(KEY_WIFI_TETHER_CLIENT_MANAGER);
                     }
                     return keys;
                 }

src/com/android/settings/wifi/tether/WifiTetherClientManagerPreferenceController.java
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.wifi.tether;
+
+import android.annotation.NonNull;
+import android.content.Context;
+import android.net.MacAddress;
+import android.net.wifi.SoftApCapability;
+import android.net.wifi.SoftApConfiguration;
+import android.net.wifi.WifiManager;
+import android.util.FeatureFlagUtils;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.FeatureFlags;
+import com.android.settings.wifi.tether.WifiTetherBasePreferenceController;
+
+import java.util.List;
+
+public class WifiTetherClientManagerPreferenceController extends WifiTetherBasePreferenceController
+        implements WifiManager.SoftApCallback {
+
+    public static final String DEDUP_POSTFIX = "_2";
+    public static final String PREF_KEY = "wifi_tether_client_manager";
+
+    private boolean mSupportForceDisconnect;
+
+    public WifiTetherClientManagerPreferenceController(Context context,
+            WifiTetherBasePreferenceController.OnTetherConfigUpdateListener listener) {
+        super(context, listener);
+
+        mWifiManager.registerSoftApCallback(context.getMainExecutor(), this);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return FeatureFlagUtils.isEnabled(mContext, FeatureFlags.TETHER_ALL_IN_ONE)
+                ? PREF_KEY + DEDUP_POSTFIX : PREF_KEY;
+    }
+
+    @Override
+    public void onCapabilityChanged(@NonNull SoftApCapability softApCapability) {
+        mSupportForceDisconnect =
+                softApCapability.areFeaturesSupported(
+                    SoftApCapability.SOFTAP_FEATURE_CLIENT_FORCE_DISCONNECT);
+        mWifiManager.unregisterSoftApCallback(this);
+        updateDisplay();
+    }
+
+    @Override
+    public void updateDisplay() {
+        if (mPreference != null) {
+            if (mSupportForceDisconnect) {
+                mPreference.setSummary(R.string.wifi_hotspot_client_manager_summary);
+            } else {
+                mPreference.setSummary(R.string.wifi_hotspot_client_manager_list_only_summary);
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
+    }
+
+    public void updateConfig(SoftApConfiguration.Builder builder) {
+        if (builder == null || !mSupportForceDisconnect) return;
+        final SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
+        final int maxNumberOfClients = softApConfiguration.getMaxNumberOfClients();
+        final List<MacAddress> blockedClientList = softApConfiguration.getBlockedClientList();
+        builder.setMaxNumberOfClients(maxNumberOfClients)
+                .setBlockedClientList(blockedClientList);
+    }
+}

src/com/android/settings/wifi/tether/preference/WifiTetherClientLimitPreference.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.wifi.tether.preference;
+
+import android.content.Context;
+import android.widget.SeekBar;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.settings.R;
+import com.android.settings.SeekBarDialogPreference;
+
+public class WifiTetherClientLimitPreference extends SeekBarDialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private Context mContext;
+    private SeekBar mSeekBar;
+    private int mValue;
+    private int mMin;
+    private int mMax;
+
+    public WifiTetherClientLimitPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        setText(getSummaryForValue(progress + mMin));
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mSeekBar = getSeekBar(view);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mSeekBar.setMax(mMax - mMin);
+        mSeekBar.setProgress(mValue - mMin);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            setValue(mSeekBar.getProgress() + mMin, true);
+        }
+    }
+
+    private String getSummaryForValue(int value) {
+        return mContext.getResources().getQuantityString(
+            R.plurals.wifi_hotspot_client_limit_summary, value, value);
+    }
+
+    public void setMin(int min) {
+        mMin = min;
+    }
+
+    public void setMax(int max) {
+        mMax = max;
+    }
+
+    public void setValue(int value, boolean callListener) {
+        if (value == 0) value = mMax;
+        mValue = value;
+        String summary = getSummaryForValue(value);
+        setSummary(summary);
+        setText(summary);
+        if (callListener) callChangeListener(value);
+    }
+
+    public int getValue() {
+        return mValue;
+    }
+}